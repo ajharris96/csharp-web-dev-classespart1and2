73ec584524c1d334bb460f219a85cac66fbef472
andrew <andrewjharris96@gmail.com>
2021-09-21 19:24:36 -0400

    fixing branch

--- SchoolPractice/Program.cs
+++ SchoolPractice/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace SchoolPractice
 {
@@ -7,13 +6,7 @@ namespace SchoolPractice
     {
         public static void Main(string[] args)
         {
-            // TODO: Instantiate your objects and test your exercise solutions with print statements here.
-            Student andrew = new Student();
-            andrew.Name = "andrew";
-            andrew.StudentId = 1234;
-            andrew.NumberOfCredits = 1;
-            andrew.Gpa = 4.0;
-
+            // TODO: Test your exercise solutions with print statements here.
         }
     }
 }
--- SchoolPractice/Teacher.cs
+++ SchoolPractice/Teacher.cs
@@ -3,17 +3,18 @@ namespace SchoolPractice
 {
     public class Teacher
     {
-<<<<<<< HEAD
-        public Teacher()
-=======
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Subject { get; set; }
         public int YearsTeaching { get; set; }
 
         public Teacher(string firstName, string lastName, string subject, int yearsTeaching)
->>>>>>> origin/classes2-exercises-start
         {
+            FirstName = firstName;
+            LastName = lastName;
+            Subject = subject;
+            YearsTeaching = yearsTeaching;
         }
+
     }
 }
